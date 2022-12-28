ffcb880abc43b8482a9aa2b10cf0f9f6f3af226c
vreytor <69092748+vreytor@users.noreply.github.com>
2022-12-27 23:35:06 -0500

    test

--- models/server.js
+++ models/server.js
@@ -44,11 +44,6 @@ class Server {
         
         this.app.use( this.authPath, require('../routes/auth'));
         this.app.use( this.usuariosPath, require('../routes/usuarios'));
-        this.app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "http://backapipoke-production.up.railway.app/"); // update to match the domain you will make the request from
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-        });
     }
 
     listen() {
