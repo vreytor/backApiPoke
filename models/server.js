99fbc9cf9cfc7b9726328475a0730e848840a101
vreytor <69092748+vreytor@users.noreply.github.com>
2022-12-27 23:19:18 -0500

    test

--- models/server.js
+++ models/server.js
@@ -7,7 +7,6 @@ class Server {
 
     constructor() {
         this.app  = express();
-        this.app.use(cors());
         this.port = process.env.PORT;
 
         this.usuariosPath = '/api/usuarios';
@@ -31,7 +30,13 @@ class Server {
     middlewares() {
 
         // CORS
-        //this.app.use( cors() );
+        this.app.use( cors() );
+
+        this.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "http://backapipoke-production.up.railway.app/"); // update to match the domain you will make the request from
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
 
         // Lectura y parseo del body
         this.app.use( express.json() );
