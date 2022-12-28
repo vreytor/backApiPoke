ccb4d1e6d30b3bf169400c072537c1e048b61de8
vreytor <69092748+vreytor@users.noreply.github.com>
2022-12-27 23:48:33 -0500

    test

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,12 @@ const Usuario = require('../models/usuario');
 
 const login = async(req, res = response) => {
 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+
     const { correo, password } = req.body;
 
     try {
